1635d625271247000fb1a4e55d6f38fae42b7739
Artem Vazhentsev <vazhentsev@airi.net>
2024-04-09 15:03:25 +0300

    added lm-polygraph

---
+++ lm-polygraph/src/lm_polygraph/generation_metrics/__init__.py
@@ -0,0 +1,6 @@
+from .rouge import RougeMetric
+from .model_score import ModelScoreSeqMetric, ModelScoreTokenwiseMetric
+from .bart_score import BartScoreSeqMetric
+from .accuracy import AccuracyMetric
+from .comet import Comet
+from .alignscore import AlignScore
