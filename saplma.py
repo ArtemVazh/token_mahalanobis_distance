cc3bc4350df1739d2e5d759425950fe4ce054cc8
Artem Vazhentsev <vazhentsev@airi.net>
2024-08-23 10:16:33 +0300

    added saplma

---
+++ saplma.py
@@ -0,0 +1,158 @@
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
+
+from typing import Dict
+
+from lm_polygraph.estimators.estimator import Estimator
+from lm_polygraph.generation_metrics.aggregated_metric import AggregatedMetric
+
+from lm_polygraph.estimators.mahalanobis_distance import (
+    compute_inv_covariance,
+    mahalanobis_distance_with_known_centroids_sigma_inv,
+    MahalanobisDistanceSeq,
+    create_cuda_tensor_from_numpy,
+    JITTERS
+)
+
+
+class MLP_NN(nn.Module):
+    def __init__(self, n_features: int = 4096, regression: bool = False):
+        super().__init__()
+        self.layers = nn.ModuleList([nn.Linear(n_features, 256),
+                                     nn.ReLU(),
+                                     nn.Linear(256, 128),
+                                     nn.ReLU(),
+                                     nn.Linear(128, 64),
+                                     nn.ReLU(),
+                                     nn.Linear(64, 1)])
+        if regression:
+            self.activation = nn.Identity()
+        else:
+            self.activation = nn.Sigmoid()
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return self.activation(x)
+
+class MLP:
+    def __init__(self, 
+                 n_features: int = 4096, 
+                 n_epochs: int = 5,
+                 batch_size: int = 64,
+                 regression: bool = False
+                ):
+        self.n_epochs = n_epochs
+        self.batch_size = batch_size
+        if regression:
+            self.loss = nn.MSELoss()
+        else:
+            self.loss = nn.BCELoss()
+        self.model = MLP_NN(n_features, regression)
+        self.optimizer = optim.AdamW(self.model.parameters())
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    def fit(self, X, y):
+        X_torch = torch.tensor(X, dtype=torch.float32)
+        y_torch = torch.tensor(y, dtype=torch.float32).reshape(-1, 1)
+        batch_start = torch.arange(0, len(X), self.batch_size)
+        self.model.to(self.device)
+        for epoch in tqdm(range(self.n_epochs)):
+            self.model.train()
+            for start in batch_start:
+                X_batch = X_torch[start:start+self.batch_size].to(self.device)
+                y_batch = y_torch[start:start+self.batch_size].to(self.device)
+                y_pred = self.model(X_batch)
+                loss = self.loss(y_pred, y_batch)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+                
+    def predict(self, X):
+        X_torch = torch.tensor(X, dtype=torch.float32)
+        batch_start = torch.arange(0, len(X), self.batch_size)
+        self.model.eval()
+        prediction = []
+        if next(self.model.parameters()).device.type != self.device.type:
+            self.model.to(self.device)
+        for start in batch_start:
+            X_batch = X_torch[start:start+self.batch_size].to(self.device)
+            y_pred = self.model(X_batch)
+            prediction.append(y_pred.cpu().detach().flatten())
+        prediction = np.concatenate(prediction)
+        return prediction
+
+
+class SAPLMA(Estimator):
+    def __init__(
+        self,
+        embeddings_type: str = "decoder",
+        parameters_path: str = None,
+        normalize: bool = False,
+        aggregation: str = "mean",
+        hidden_layer: int = -1,
+        metric = None,
+        metric_name: str = "",
+        aggregated: bool = False,
+        device: str = "cuda"
+    ):
+        self.hidden_layer = hidden_layer
+        if self.hidden_layer == -1:
+            super().__init__(["train_embeddings", "embeddings", "train_greedy_tokens", "train_target_texts"], "sequence")
+        else:
+            super().__init__([f"train_embeddings_{self.hidden_layer}", f"embeddings_{self.hidden_layer}", "train_greedy_tokens", "train_target_texts"], "sequence")
+        self.centroid = None
+        self.sigma_inv = None
+        self.parameters_path = parameters_path
+        self.embeddings_type = embeddings_type
+        self.normalize = normalize
+        self.min = 1e100
+        self.max = -1e100
+        self.is_fitted = False
+        self.aggregation = aggregation
+        self.metric_name = metric_name
+        self.device = device
+        self.regression = True if metric_name!="Accuracy" else False
+        self.ue_predictor = MLP(regression=self.regression)
+        if metric is not None:
+            self.metric = metric
+            if aggregated:
+                self.metric = AggregatedMetric(base_metric=self.metric)
+
+    def __str__(self):
+        hidden_layer = "" if self.hidden_layer==-1 else f"_{self.hidden_layer}"
+        return f"SAPLMA_{self.embeddings_type}{hidden_layer} ({self.metric_name})"
+
+    def __call__(self, stats: Dict[str, np.ndarray], save_data: bool = True) -> np.ndarray:
+        # take the embeddings
+        if self.hidden_layer == -1:
+            hidden_layer = ""
+        else:
+            hidden_layer = f"_{self.hidden_layer}"
+        embeddings = create_cuda_tensor_from_numpy(
+            stats[f"embeddings_{self.embeddings_type}{hidden_layer}"]
+        )
+        
+        # compute centroids if not given
+        if not self.is_fitted:
+            train_greedy_texts = stats[f"train_greedy_texts"]
+
+            train_greedy_texts = stats[f"train_greedy_texts"]
+            train_greedy_tokens = stats[f"train_greedy_tokens"]
+            train_target_texts = stats[f"train_target_texts"]
+            self.train_seq_metrics = np.array([self.metric({"greedy_texts": [x], "target_texts": [y]}, [y], [y])[0]
+                                              for x, y, x_t in zip(train_greedy_texts, train_target_texts, train_greedy_tokens)])
+            train_embeddings = create_cuda_tensor_from_numpy(
+                stats[f"train_embeddings_{self.embeddings_type}{hidden_layer}"]
+            )
+            self.ue_predictor = MLP(n_features=train_embeddings.shape[-1], regression=self.regression)
+            self.ue_predictor.fit(train_embeddings, 1 - self.train_seq_metrics)
+            self.is_fitted = True
+                
+        ue = self.ue_predictor.predict(embeddings)
+
+        return ue
